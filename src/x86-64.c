0184673ade20636934bc56a140c8b71e5503dd64
Woohyun Joh <johwhj@proton.me>
2025-03-05 19:04:32 +0900

    fix: add unimplemented definitions

---
+++ src/x86-64.c
@@ -0,0 +1,21 @@
+/*
+ * CASM - C-like assembler
+ *
+ * Written in 2025 by Woohyun Joh <johwhj@gmail.com>
+ *
+ * To the extent possible under law, the author(s) have dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide. This software is distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication along
+ * with this software.
+ * If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+#include "casm.h"
+
+int
+casm_codegen(struct casm *casm)
+{
+	/* TODO */
+}
