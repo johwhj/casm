1af2f083a3f2005ed0733a56c1b38f19bfbaf1ba
Woohyun Joh <johwhj@proton.me>
2025-03-10 10:45:27 +0900

    fix: remove useless fields and function

--- src/casm.h
+++ src/casm.h
@@ -16,21 +16,16 @@
 #define _CASM_H
 
 #define PROGNAME "casm"
-#define BUF_SIZE 4096
 
 #include <stdio.h>
 
 struct casm {
-	FILE *src, *obj;
-	long col, row;
-	size_t idx, len;
-	char buf[BUF_SIZE]; /* contains \0 at BUF_SIZE - 1 */
 	const char *name;
+	FILE *src, *obj;
 };
 
 int casm_init(struct casm *, const char *); /* init.c */
 int casm_codegen(struct casm *);            /* x86-64.c */
-char *casm_token(struct casm *);            /* token.c */
 void casm_free(struct casm *);              /* free.c */
 
 /* utils.c */
