15943e3ea3ed9ec8f7efb4b99697136da5341d10
Woohyun Joh <johwhj@gmail.com>
2025-03-23 21:12:44 +0900

    fix: add token types to check

--- test/lexer.c
+++ test/lexer.c
@@ -12,7 +12,8 @@ main(void)
 	lexer_init(&lex, src);
 
 	while ((tok = lexer_token(&lex)).type != TOKEN_EOF) {
-	     	if (tok.type == TOKEN_UNKNOWN || tok.type == TOKEN_TYPE)
+		if (tok.type == TOKEN_NAME || tok.type == TOKEN_TYPE
+		    || tok.type == TOKEN_INTEGER || tok.type == TOKEN_STRING)
 	     		puts(tok.str);
 		else
 			printf("%c\n", tok.type);
