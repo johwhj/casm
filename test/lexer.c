00da2a576fa752362acbfbd218ea249b0d72f06f
Woohyun Joh <johwhj@gmail.com>
2025-03-27 09:59:41 +0900

    fix: shorten token type checking

--- test/lexer.c
+++ test/lexer.c
@@ -6,8 +6,7 @@ is_printable(enum token_type type)
 {
 	return type == TOKEN_NAME || type == TOKEN_TYPE
 	       || type == TOKEN_INTEGER || type == TOKEN_STRING
-	       || type == TOKEN_FLOAT || type == TOKEN_LABEL
-	       || type == TOKEN_MACRO;
+	       || type == TOKEN_FLOAT || type == TOKEN_MACRO;
 }
 
 int
