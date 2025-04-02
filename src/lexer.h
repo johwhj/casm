daca46f9d0a0d9af21a297e9193b5544b5c3229a
Woohyun Joh <johwhj@gmail.com>
2025-04-02 23:21:26 +0900

    fix: separate lexer from casm.h

---
+++ src/lexer.h
@@ -0,0 +1,31 @@
+/*
+ * CASM - C-like assembler
+ *
+ * Written in 2025 by Woohyun Joh <johwhj@gmail.com>
+ *
+ * To the extent possible under law, the author(s) have dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide. This software is distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication along
+ * with this software.
+ * If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+#include <stdio.h>
+
+struct lexer {
+	char *str;
+	size_t col, row;
+};
+
+enum token_type {
+	TOKEN_NAME,
+	TOKEN_PUNC,
+	TOKEN_STRING,
+	TOKEN_NUMBER,
+	TOKEN_EOF
+};
+
+struct lexer lexer_new(FILE *);
+enum token_type lexer_next(struct lexer *, char *, size_t);
